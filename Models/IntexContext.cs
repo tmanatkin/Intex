a4314939b2eddedda86bfea83fa928eb63bff5ab
tanneratkin <tmanatkin@gmail.com>
2024-04-10 10:06:50 -0600

    hopefully merge with caleb and we dont die. changed css and layout. removed connection string

--- Models/IntexContext.cs
+++ Models/IntexContext.cs
@@ -24,8 +24,8 @@ public partial class IntexContext : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:tanner-atkin-azure-server.database.windows.net,1433;Initial Catalog=AzureDB;Persist Security Info=False;User ID=tmanatkin;Password=ChevronzThatzFlatwormzSenatorzHalt0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
