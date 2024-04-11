7d44faf09960513b4deae059a3de2e336e51dc33
JoeFlake <flakejoe8@gmail.com>
2024-04-10 23:09:59 -0600

    brought in caleb's css

--- Models/ProductDetailViewModel.cs
+++ Models/ProductDetailViewModel.cs
@@ -4,5 +4,10 @@
     {
         public Product Product { get; set; }
         public ItemRecommendation Recommendations { get; set; }
+        //public string GetImgLink(int productId)
+        //{
+        //    var product = Products.FirstOrDefault(p => p.ProductId == productId);
+        //    return product != null ? product.ImgLink : string.Empty;
+        //}
     }
 }
