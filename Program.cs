51dee7f5dc96bdced30b55112545b54ace7a1191
tanneratkin <tmanatkin@gmail.com>
2024-04-10 18:25:42 -0600

    user creation and storing works!!!!! no roles yet

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Intex.Models;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+using Intex.Areas.Identity.Data;
 
 // builder.Services.AddDbContext<IntexContext>(options =>
 // {
 //     options.UseSqlServer(builder.Configuration["ConnectionStrings:IntexConnection"]);
 // });
+
+var builder = WebApplication.CreateBuilder(args);
+
+// var identityConnectionString = builder.Configuration.GetConnectionString("IntexIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'IntexIdentityDbContextConnection' not found.");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 // key vault stuff
 ConfigurationBuilder azureBuilder = new ConfigurationBuilder();
@@ -19,6 +23,9 @@ IConfiguration configuration = azureBuilder.Build();
 string connectionString = configuration["IntexConnectionString"];
 // Console.WriteLine($"Connection string: {connectionString}");
 
+// identity tables
+builder.Services.AddDbContext<IntexIdentityDbContext>(options => options.UseSqlServer(connectionString));
+
 var dbContextOptions = new DbContextOptionsBuilder<IntexContext>()
     .UseSqlServer(connectionString)
     .Options;
@@ -26,6 +33,9 @@ var dbContextOptions = new DbContextOptionsBuilder<IntexContext>()
 builder.Services.AddSingleton(new IntexContext(dbContextOptions));
 
 builder.Services.AddScoped<IIntexRepository, EFIntexRepository>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<IntexIdentityDbContext>();
 
 var app = builder.Build();
 
@@ -42,10 +52,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
