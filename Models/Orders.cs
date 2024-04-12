b27b7d328094c2ea48728913ec313967b6f700cc
JoeFlake <flakejoe8@gmail.com>
2024-04-11 21:02:55 -0600

    Checkout changes and also big break on user based recs

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using static Intex.Models.Cart;
 namespace Intex.Models;
 public partial class Orders
 {
     public int? TransactionId { get; set; }
+
+    [BindNever]
+    public ICollection<CartLine> Lines { get; set; }
+            = new List<CartLine>();
+
     [Required]
     public int? CustomerId { get; set; }
     [Required]
